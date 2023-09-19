0a5511e3cc7fb7a4b9d4c6d8608444b526e2ad27
Marcelo Osorio <insertpax.hot@gmail.com>
2023-09-18 23:03:32 -0400

    se arreglaron estilos y desarrollo

--- optionBoxing.js
+++ optionBoxing.js
@@ -6,6 +6,13 @@ ulContent.addEventListener("click",e => {
     infoImtem.value = e.target;
 })
 
-function name(params) {
-    
+function repeatFooter() {
+    let count = 0;
+    const interval = setInterval(() => {
+        console.log('footer '+count);
+        count++;
+        if(count === 10) clearInterval(interval);
+    }, 2000);
 }
+
+repeatFooter();
